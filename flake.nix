a18bf900f3d3ae18f2090e7464007124fddba127
Kelvin Steiner <me@steinerkelvin.dev>
2023-07-09 17:18:02 -0300

    add `naersk` flake

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, flake-utils, naersk, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+
+        naersk' = pkgs.callPackage naersk {};
+      in
+      {
+        # For `nix build` & `nix run`:
+        packages.default = naersk'.buildPackage {
+          src = ./.;
+          buildInputs = [
+            pkgs.openssl
+          ];
+          nativeBuildInputs = [
+            pkgs.pkg-config
+          ];
+        };
+
+        # For `nix develop`:
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.openssl
+          ];
+          nativeBuildInputs = [
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.pkg-config
+          ];
+        };
+      }
+    );
+}
