ca0749de7ac8766e174473caf075a95f926c0a7e
Kelvin Steiner <me@steinerkelvin.dev>
2025-01-06 09:58:25 -0300

    make naersk follow nixpkgs input; flake bump

--- flake.nix
+++ flake.nix
@@ -1,44 +1,46 @@
 {
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nix-community/naersk";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, flake-utils, naersk, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = (import nixpkgs) {
-          inherit system;
-        };
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = (import nixpkgs) {
+            inherit system;
+          };
 
-        naersk' = pkgs.callPackage naersk {};
-      in
-      {
-        # For `nix build` & `nix run`:
-        packages.default = naersk'.buildPackage {
-          src = ./.;
-          buildInputs = [
-            pkgs.openssl
-          ];
-          nativeBuildInputs = [
-            pkgs.pkg-config
-          ];
-        };
+          naersk' = pkgs.callPackage naersk { };
+        in
+        {
+          # For `nix build` & `nix run`:
+          packages.default = naersk'.buildPackage {
+            src = ./.;
+            buildInputs = [
+              pkgs.openssl
+            ];
+            nativeBuildInputs = [
+              pkgs.pkg-config
+            ];
+          };
 
-        # For `nix develop`:
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.openssl
-          ];
-          nativeBuildInputs = [
-            pkgs.pkg-config
-            # pkgs.rustc
-            # pkgs.cargo
-          ];
-        };
-      }
-    ) // {
+          # For `nix develop`:
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.openssl
+            ];
+            nativeBuildInputs = [
+              pkgs.pkg-config
+              # pkgs.rustc
+              # pkgs.cargo
+            ];
+          };
+        }
+      ) // {
       nixosModules.k-ddns = import ./nix/modules/k-ddns.nix;
     };
 }
